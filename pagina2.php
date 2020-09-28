631c4e91cd70195353d96f395abe0c16e97bc7f7
vale2904 <70981266+vale2904@users.noreply.github.com>
2020-09-28 13:23:14 -0500

    crrec
    otra corecc

--- pagina2.php
+++ pagina2.php
@@ -13,8 +13,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
          <img src="img/icone.png" width="30" height="30" alt="" loading="lazy">
-          Valeria's Bookstore
-        </a>
+          Valeria's Bookstore </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
